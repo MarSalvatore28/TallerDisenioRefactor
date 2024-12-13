c3a9ce9a32200317cd81c9005d6560ed70b2d5a4
DhamarPatino <152041769+DhamarPatino@users.noreply.github.com>
2024-12-12 23:47:20 -0500

    cambioa en las 3 clases de empleado

--- RefactorCodeSolutionMarGroup/src/main/java/ec/edu/espol/refactorcodesolutionmargroup/EmpleadoTemporario.java
+++ RefactorCodeSolutionMarGroup/src/main/java/ec/edu/espol/refactorcodesolutionmargroup/EmpleadoTemporario.java
@@ -2,32 +2,27 @@ package ec.edu.espol.refactorcodesolutionmargroup;
 
 public class EmpleadoTemporario extends Empleado {
 
-    private String nombre;
-    private String genero;
-    private double salarioBase;
-    private int horasTrabajadas;
-    private String departamento;
     private int mesesContrato;
+    private String genero;
 
     public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, String departamento, int mesesContrato, String genero) {
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-        this.horasTrabajadas = horasTrabajadas;
-        this.departamento = departamento;
+        super(nombre, salarioBase, horasTrabajadas, departamento);
         this.mesesContrato = mesesContrato;
         this.genero = genero;
     }
 
+    @Override
     public void imprimirDetalles() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Genero: " + super.getNombre());
-        System.out.println("Salario: " + salarioBase);
-        System.out.println("Horas trabajadas: " + horasTrabajadas);
-        System.out.println("Departamento: " + departamento);
-        System.out.println("Meses de contrato: " + mesesContrato);
+        super.imprimirDetalles();
+        System.out.println("Meses de Contrato: " + mesesContrato);
+        System.out.println("Género: " + genero);
     }
-
     // Más metodos
+
+    @Override
+    public double calcularSalarioAdicional() {
+        return getHorasTrabajadas() > 40 ? (getHorasTrabajadas() - 40) * 50 : 0; // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 
 
