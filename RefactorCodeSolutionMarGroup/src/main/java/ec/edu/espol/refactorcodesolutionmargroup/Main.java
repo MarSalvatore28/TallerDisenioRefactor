f3e839eb7b79284f4910bf344b8791acba1fb9fa
MARLO VALLE <mcvalle@espol.edu.ec>
2024-12-12 23:49:21 -0500

    Update Main.java

--- RefactorCodeSolutionMarGroup/src/main/java/ec/edu/espol/refactorcodesolutionmargroup/Main.java
+++ RefactorCodeSolutionMarGroup/src/main/java/ec/edu/espol/refactorcodesolutionmargroup/Main.java
@@ -11,6 +11,24 @@ package ec.edu.espol.refactorcodesolutionmargroup;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Empresa empresa = new Empresa();
+
+        
+        EmpleadoPorHoras emp1 = new EmpleadoPorHoras("Dario Laborde", 45, 15, "Sistemas", "Masculino");
+        EmpleadoFijo emp2 = new EmpleadoFijo("Jorge Gaibor", 700, 45, "Contabilidad", 40, "Masculino");
+        EmpleadoTemporario emp3 = new EmpleadoTemporario("Jordan Salinas", 200, 20, "Medico", 6, "Masculino");
+
+        
+        empresa.contratarEmpleado(emp1);
+        empresa.contratarEmpleado(emp2);
+        empresa.contratarEmpleado(emp3);
+
+        
+        emp1.imprimirDetalles();
+        System.out.println("====================================");
+        emp2.imprimirDetalles();
+        System.out.println("====================================");
+        emp3.imprimirDetalles();
     }
 }
